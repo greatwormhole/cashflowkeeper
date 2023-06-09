c3d56a19738328cd29ad9aef7abb07bca5da0797
Slava <vvkurgan@mail.ru>
2023-06-09 23:48:44 +0300

    Теперь при пустой БД home_page работает без ошибок. Добавлены проверки наличия данных в колонках.

--- apps/home/views.py
+++ apps/home/views.py
@@ -8,6 +8,7 @@ import datetime
 from datetime import datetime
 import json
 
+
 def home_page(request):
     # получение текущего года и месяца
     curr_year = str(datetime.now().year)
@@ -20,42 +21,79 @@ def home_page(request):
         previous_month = '12'
         previous_year = str(datetime.now().year - 1)
 
+    if Transactions.objects.all().filter(date__year=curr_year, date__month=curr_month,
+                                                    type='Income').count() != 0:
+        total_incomes = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
+                                                    type='Income').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_incomes = 0
 
-    # вывод total'ов в текущем месяце
-    total_incomes = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
-                                                type='Income').aggregate(Sum("amount"))['amount__sum']
-    total_outcomes = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
-                                                type='Outcome').aggregate(Sum("amount"))['amount__sum']
-    total_investments = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
-                                                type='Investment').aggregate(Sum("amount"))['amount__sum']
-    # инвестировано за все время
-    all_time_investments = Transactions.objects.filter(type='Investment').aggregate(Sum("amount"))['amount__sum']
+    if Transactions.objects.all().filter(date__year=curr_year, date__month=curr_month,
+                                                     type='Outcome').count() != 0:
+        total_outcomes = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
+                                                     type='Outcome').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_outcomes = 0
+
+    if Transactions.objects.all().filter(date__year=curr_year, date__month=curr_month,
+                                                        type='Investment').count() != 0:
+        total_investments = Transactions.objects.filter(date__year=curr_year, date__month=curr_month,
+                                                        type='Investment').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_investments = 0
+
+        # инвестировано за все время
+    if Transactions.objects.all().filter(type='Investment').count() != 0:
+        all_time_investments = Transactions.objects.filter(type='Investment').aggregate(Sum("amount"))['amount__sum']
+    else:
+        all_time_investments = 0
 
     # динамика к предыдущему месяцу
-    total_incomes_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
-                                                type='Income').aggregate(Sum("amount"))['amount__sum']
+    if Transactions.objects.all().filter(date__year=previous_year, date__month=previous_month,
+                                                         type='Income').count() != 0:
+        total_incomes_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
+                                                         type='Income').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_incomes_prev = 1
+
     incomes_perc = round((total_incomes - total_incomes_prev) * 100 / total_incomes_prev, 1)
 
-    total_outcomes_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
-                                                type='Outcome').aggregate(Sum("amount"))['amount__sum']
+    if Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
+                                                          type='Outcome').all().count() != 0:
+        total_outcomes_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
+                                                          type='Outcome').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_outcomes_prev = 1
+
     outcomes_perc = round((total_outcomes - total_outcomes_prev) * 100 / total_outcomes_prev, 1)
 
-    total_investments_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
-                                                type='Investment').aggregate(Sum("amount"))['amount__sum']
+    if Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
+                                                         type='Investment').all().count() != 0:
+        total_investments_prev = Transactions.objects.filter(date__year=previous_year, date__month=previous_month,
+                                                         type='Investment').aggregate(Sum("amount"))['amount__sum']
+    else:
+        total_investments_prev=1
     investments_perc = round((total_investments - total_investments_prev) * 100 / total_investments_prev, 1)
 
     # bar chart
     # работа с датами
     dates_list = Transactions.objects.values_list('date')
-    min_date = dates_list.order_by('date').first()[0]
-    max_date = dates_list.order_by('date').last()[0]
-    min_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').first()[0].strftime(
+    if dates_list.all().count() != 0:
+        min_date = dates_list.order_by('date').first()[0]
+        max_date = dates_list.order_by('date').last()[0]
+        min_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').first()[0].strftime(
         "%Y-%m-%d")
-    max_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').last()[0].strftime(
+        max_date_str: object = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').last()[0].strftime(
         "%Y-%m-%d")
+    else:
+        min_date = '2023-01-01'
+        max_date = '2023-01-01'
+        min_date_str = '2023-01-01'
+        max_date_str = '2023-01-31'
 
     # работа с транзакциями
-    transaction = Transactions.objects.filter(~Q(type='Investment')).values('date', 'type').order_by().annotate(amount=Sum('amount')).order_by('date')
+    transaction = Transactions.objects.filter(~Q(type='Investment')).values('date', 'type').order_by().annotate(
+        amount=Sum('amount')).order_by('date')
     transaction = list(transaction)
     # приведение дат к строке
     for item in transaction:
@@ -88,6 +126,7 @@ def home_page(request):
                                                'outcomes_perc': outcomes_perc,
                                                'investments_perc': investments_perc})
 
+
 def add(request):
     error = ''
     if request.method == 'POST':
@@ -105,6 +144,7 @@ def add(request):
         'error': error
     }
     return render(request, 'home/add.html', data)
+
 
 def add_category(request):
     error = ''
@@ -126,18 +166,19 @@ def add_category(request):
 
 
 def statistics(request):
-
     # работа со временем
     curr_year = str(datetime.now().year)
     curr_month = str(datetime.now().month)
 
     # bar chart
-        # работа с датами
+    # работа с датами
     dates_list = Transactions.objects.values_list('date')
     min_date = dates_list.order_by('date').first()[0]
     max_date = dates_list.order_by('date').last()[0]
-    min_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').first()[0].strftime("%Y-%m-%d")
-    max_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').last()[0].strftime("%Y-%m-%d")
+    min_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').first()[0].strftime(
+        "%Y-%m-%d")
+    max_date_str = dates_list.filter(date__year=curr_year, date__month=curr_month).order_by('date').last()[0].strftime(
+        "%Y-%m-%d")
 
     # работа с транзакциями
     transaction = Transactions.objects.values('date', 'type').order_by().annotate(amount=Sum('amount')).order_by('date')
@@ -159,21 +200,24 @@ def statistics(request):
     transaction_df['date'] = transaction_df['date'].dt.strftime("%Y-%m-%d")
 
     # группировка датафрейма
-    transaction_df = transaction_df.groupby(['date', 'type'], as_index=False).agg(sum_of_amount=('amount', 'sum')).sort_values(by='date')
+    transaction_df = transaction_df.groupby(['date', 'type'], as_index=False).agg(
+        sum_of_amount=('amount', 'sum')).sort_values(by='date')
     transaction_json = transaction_df.to_json(orient='records')
     transaction = json.loads(transaction_json)
 
     # doughnut chart
-    doughnut = Transactions.objects.filter(date__year=curr_year, date__month=curr_month).values('type', 'category').order_by().annotate(sum_of_amount=Sum('amount')).order_by('-sum_of_amount')
+    doughnut = Transactions.objects.filter(date__year=curr_year, date__month=curr_month).values('type',
+                                                                                                'category').order_by().annotate(
+        sum_of_amount=Sum('amount')).order_by('-sum_of_amount')
     # данные для сводки по категориям
     by_category = Transactions.objects.filter(date__year=curr_year,
                                               date__month=curr_month).values('type', 'category') \
-                                      .order_by().annotate(sum_of_amount=Sum('amount'),
-                                                           count=Count('amount')).order_by('-sum_of_amount')
+        .order_by().annotate(sum_of_amount=Sum('amount'),
+                             count=Count('amount')).order_by('-sum_of_amount')
     # данные по total метрикке
     total = Transactions.objects.filter(date__year=curr_year,
-                                              date__month=curr_month).values('type') \
-                                      .order_by().annotate(sum_of_amount=Sum('amount'))
+                                        date__month=curr_month).values('type') \
+        .order_by().annotate(sum_of_amount=Sum('amount'))
 
     # получение предыдущего месяца
     if datetime.now().month != 1:
@@ -217,8 +261,9 @@ def statistics(request):
 def history(request):
     return render(request, 'home/history.html')
 
+
 def profile(request):
-    error=''
+    error = ''
     if request.method == 'POST':
         form = ProfileForm(request.POST)
         if form.is_valid():
@@ -234,8 +279,9 @@ def profile(request):
     }
     return render(request, 'home/profile.html', data)
 
+
 def create(request):
-    error=''
+    error = ''
     if request.method == 'POST':
         form = ProfileForm(request.POST)
         if form.is_valid():
